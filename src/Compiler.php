1ee11bbedde2f440f2b885f13f1db69a996c5560
hacpaka <hacpaka@eggbe.com>
2018-05-23 22:03:51 +0300

    allowed inline statements; added standard constructions

--- src/Compiler.php
+++ src/Compiler.php
@@ -58,15 +58,19 @@ class Compiler {
 
 			if (!empty($line = rtrim($line))) {
 				try {
-					yield preg_replace_callback('/(\W|\A)@(' . Reglib::KEYWORD . ')\s*(.*)$/s', function ($Matches) {
-						return $Matches[1] . (!empty($Matches[1]) ? ' ' : '') . $this->process(strtolower($Matches[2]), $this->parse($Matches[3])) . $Matches[3];
-					}, $line);
-
+					yield $this->parse($line);
 				} catch (\Exception $Exception){
-					throw new \Exception('Error in ' . (string)$File . ' on ' . $index . ': ' . $Exception->getMessage());
+					throw new \Exception('Error in ' . (string)$Reader . ' on ' . $index . ': ' . $Exception->getMessage());
 				}
 			}
 		}
+	}
+
+	protected final function parse(string $line) : string {
+		return preg_replace_callback('/(\W|\A)@(' . Reglib::KEYWORD . ')\s*(.*)$/s', function ($Matches) {
+			return $Matches[1] . (!preg_match('/\s+$/', $Matches[1]) ? ' ' : '')
+				. "<?php " . $this->process(strtolower($Matches[2]), $this->analize($Matches[3])) . " ?>" . $this->parse($Matches[3]);
+		}, $line);
 	}
 
 	/**
@@ -105,13 +109,9 @@ class Compiler {
 	 * @return string
 	 * @throws \Exception
 	 */
-	protected final function parse(string &$source): string {
-		if (empty($source)) {
+	protected final function analize(string &$source): string {
+		if (empty($source) || !preg_match('/^\(/', $source)) {
 			return $source;
-		}
-
-		if (!preg_match('/^\(/', $source)) {
-			throw new \Exception('Invalid separator!');
 		}
 
 		$original = $source;
@@ -140,3 +140,23 @@ class Compiler {
 		return preg_replace('/' . preg_quote($source, '/'). '$/', '', $original);
 	}
 }
+
+/** @noinspection PhpUnhandledExceptionInspection */
+Compiler::register(new SSignature('if', function ($condition) {
+	return 'if ' . $condition . '{';
+}));
+
+/** @noinspection PhpUnhandledExceptionInspection */
+Compiler::extend('if', new SSignature('else', function ($condition) {
+	return '} else {';
+}));
+
+/** @noinspection PhpUnhandledExceptionInspection */
+Compiler::register(new SSignature('for', function ($condition) {
+	return 'for ' . $condition . '{';
+}));
+
+/** @noinspection PhpUnhandledExceptionInspection */
+Compiler::register(new SSignature('foreach', function ($condition) {
+	return 'foreach ' . $condition . '{';
+}));
