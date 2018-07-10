f20f577854ba13b9a833b3b0a308bb05f00bcd71
hacpaka <hacpaka@eggbe.com>
2018-07-10 14:26:09 +0300

    Brackets parser became sensitive to the brackets type

--- src/Parsers/BracketsParser.php
+++ src/Parsers/BracketsParser.php
@@ -6,6 +6,8 @@ use \Able\Prototypes\TStatic;
 use \Able\Reglib\Regexp;
 use \Able\Reglib\Reglib;
 
+use \Able\Helpers\Src;
+
 class BracketsParser {
 	use TStatic;
 
@@ -22,29 +24,48 @@ class BracketsParser {
 	}
 
 	/**
+	 * @const string
+	 */
+	public const BT_CIRCLE = '()';
+
+	/**
+	 * @const string
+	 */
+	public const BT_CURLY = '{}';
+
+	/**
+	 * @const string
+	 */
+	public const BT_SQUARE = '[]';
+
+	/**
 	 * @param string $source
+	 * @param string $type
 	 * @param int $count
 	 * @return string
 	 * @throws \Exception
 	 */
-	public static function parse(string &$source, int $count = 0): string {
-		if (!preg_match('/^\s*\(/', $source) && $count < 1) {
+	public static function parse(string &$source, string $type = self::BT_CIRCLE, int $count = 0): string {
+		if (!in_array($type, [self::BT_CIRCLE, self::BT_CURLY, self::BT_SQUARE])){
+			throw new \Exception('Invalid brackets type!');
+		}
+
+		if (!preg_match($e = '/^' . preg_quote($type[0], '/') . '/', $source) && $count < 1) {
 			return '';
 		}
 
 		$out = '';
-
 		do{
-			if (!empty($source) && $source[0] == '(') {
+			if (!empty($source) && $source[0] == $type[0]) {
 				$count++;
 			}
 
-			if (!empty($source) && $source[0] == ')') {
+			if (!empty($source) && $source[0] == $type[1]) {
 				$count--;
 			}
 
-			$out .= Regexp::create('/^[()]{0,1}' . ($count > 0 ? '(?:'. Reglib::QUOTED
-				. '|[^)(]+)*\s*' : '') . '/')->retrieve($source) ;
+			$out .= Regexp::create($e = '/^[' . Src::esc($type, ']') . ']{0,1}' . ($count > 0 ? '(?:'. Reglib::QUOTED
+				. '|[^' . Src::esc($type, ']') . ']+)*\s*' : '') . '/')->retrieve($source) ;
 
 			if (empty($source) && $count > 0 && !is_null(static::$Resolver)){
 				$source = call_user_func(static::$Resolver);
