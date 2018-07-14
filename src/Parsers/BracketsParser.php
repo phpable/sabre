373246c994571a24a194d8f1408168f9a36ed2b2
hacpaka <hacpaka@eggbe.com>
2018-07-14 15:43:52 +0300

    autodetection of a bracket type implemented

--- src/Parsers/BracketsParser.php
+++ src/Parsers/BracketsParser.php
@@ -7,6 +7,7 @@ use \Able\Reglib\Regexp;
 use \Able\Reglib\Reglib;
 
 use \Able\Helpers\Src;
+use \Able\Helpers\Arr;
 
 class BracketsParser {
 	use TStatic;
@@ -24,48 +25,69 @@ class BracketsParser {
 	}
 
 	/**
-	 * @const string
+	 * @var array
 	 */
-	public const BT_CIRCLE = '()';
+	private static $Brackets = ['(' => '()', '[' => '[]', '{' => '{}'];
 
 	/**
 	 * @const string
 	 */
-	public const BT_CURLY = '{}';
+	public const BT_DETECT = 0;
 
 	/**
 	 * @const string
 	 */
-	public const BT_SQUARE = '[]';
+	public const BT_CIRCLE = 1;
+
+	/**
+	 * @const string
+	 */
+	public const BT_SQUARE = 2;
+
+	/**
+	 * @const string
+	 */
+	public const BT_CURLY = 3;
 
 	/**
 	 * @param string $source
-	 * @param string $type
+	 * @param int $type
 	 * @param int $count
 	 * @return string
 	 * @throws \Exception
 	 */
-	public static function parse(string &$source, string $type = self::BT_CIRCLE, int $count = 0): string {
-		if (!in_array($type, [self::BT_CIRCLE, self::BT_CURLY, self::BT_SQUARE])){
+	public static function parse(string &$source, int $type = self::BT_DETECT, int $count = 0): string {
+		if ($type < self::BT_DETECT || $type > self::BT_SQUARE){
 			throw new \Exception('Invalid brackets type!');
 		}
 
-		if (!preg_match($e = '/^' . preg_quote($type[0], '/') . '/', $source) && $count < 1) {
-			return '';
+		if ($type == self::BT_DETECT && $count > 0){
+			throw new \Exception('Invalid brackets type!');
+		}
+
+		if ($type == self::BT_DETECT && ($type = array_search($source[0],
+			array_keys(self::$Brackets)) + 1) < 1){
+				throw new \Exception('Invalid brackets type!');
+		}
+
+		$pair = Arr::value(self::$Brackets, $type - 1);
+		if (!preg_match($e = '/^' . preg_quote($pair[0], '/')
+			. '/', $source) && $count < 1) {
+				return '';
 		}
 
 		$out = '';
 		do{
-			if (!empty($source) && $source[0] == $type[0]) {
+			if (!empty($source) && $source[0] == $pair[0]) {
 				$count++;
 			}
 
-			if (!empty($source) && $source[0] == $type[1]) {
+			if (!empty($source) && $source[0] == $pair[1]) {
 				$count--;
 			}
 
-			$out .= Regexp::create($e = '/^[' . Src::esc($type, ']') . ']{0,1}' . ($count > 0 ? '(?:'. Reglib::QUOTED
-				. '|[^' . Src::esc($type, ']') . ']+)*\s*' : '') . '/')->retrieve($source) ;
+			$out .= Regexp::create($e = '/^[' . Src::esc($pair, ']') . ']{0,1}' . ($count > 0 ? '(?:'. Reglib::QUOTED
+				. '|[^' . Src::esc($pair, ']') . ']+)*\s*' : '') . '/')->retrieve($source) ;
 
 			if (empty($source) && $count > 0 && !is_null(static::$Resolver)){
 				$source = call_user_func(static::$Resolver);
